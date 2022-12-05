4e0f7749b9970c7cef4ca9f622ce8f0c5485dc11
Unreal-System <31071269+Unreal-System@users.noreply.github.com>
2022-12-05 20:43:22 +0800

    修正脚本错误

--- BuildToAlpineDocker.bat
+++ BuildToAlpineDocker.bat
@@ -1,12 +1,12 @@
 @Echo off
 set base_dir=%~dp0
-del /F /S /Q ..\app\
-dotnet publish -r linux-musl-x64 -c release -o /app -p:SelfContained=true -p:PublishTrimmed=false -p:PublishReadyToRun=true -p:PublishSingleFile=true
-cd ..
+del /F /S /Q .\app\
+del /F /S /Q .\app\database.db
+dotnet publish -r linux-musl-x64 -c release -o .\app -p:SelfContained=true -p:PublishTrimmed=false -p:PublishReadyToRun=true -p:PublishSingleFile=true
 cd app
 del RBQBot.pdb /Q /F
 mkdir db
 cd ..
-cd RBQBot
-copy .\Dockerfile .\..\app\
-pause.
+copy .\Dockerfile .\app\
+echo "Build Done."
+pause
